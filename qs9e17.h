68a9aae9a5d5eb502891afff20430456720fcad2
yumetodo <yume-wikijp@live.jp>
2018-06-08 21:58:35 +0900

    chore: set file encoding to UTF-8 with BOM

--- qs9e17.h
+++ qs9e17.h
@@ -1,4 +1,4 @@
-/*=============================================================================
+﻿/*=============================================================================
   Copyright (C) 2018 河村　知行 <t-kawa@crux.ocn.ne.jp>
   Copyright (C) 2018 yumetodo <yume-wikijp@live.jp>
   Distributed under the Boost Software License, Version 1.0.
