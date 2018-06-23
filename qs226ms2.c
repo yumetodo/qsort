21447e987ff9f452d1cdefeadd989f6f4534caeb
Koichi Murase <myoga.murase@gmail.com>
2018-06-23 13:47:37 +0900

    fix(qs226ms2): replace tabs

--- qs226ms2.c
+++ qs226ms2.c
@@ -15,7 +15,7 @@
 #include <string.h>
 #include <unistd.h>
 //#include <memcpy.h>
-//_PTR	 _EXFUN(mempcpy,(_PTR, const _PTR, size_t));
+//_PTR _EXFUN(mempcpy,(_PTR, const _PTR, size_t));
 void * __mempcpy(void* dstpp, const void* srcpp, size_t len);
 typedef int (*__compar_d_fn_t) (const void *, const void *, void *);
 
@@ -54,102 +54,102 @@ msort_with_tmp (const struct msort_param *p, void *b, size_t n)
     {
     case 0:
       while (n1 > 0 && n2 > 0)
-	{
+        {
           pppppp(" *uint32_t  \n", 1)
-	  if ((*cmp) (b1, b2, arg) <= 0)
-	    {
-	      *(uint32_t *) tmp = *(uint32_t *) b1;
-	      b1 += sizeof (uint32_t);
-	      --n1;
-	    }
-	  else
-	    {
-	      *(uint32_t *) tmp = *(uint32_t *) b2;
-	      b2 += sizeof (uint32_t);
-	      --n2;
-	    }
-	  tmp += sizeof (uint32_t);
-	}
+          if ((*cmp) (b1, b2, arg) <= 0)
+            {
+              *(uint32_t *) tmp = *(uint32_t *) b1;
+              b1 += sizeof (uint32_t);
+              --n1;
+            }
+          else
+            {
+              *(uint32_t *) tmp = *(uint32_t *) b2;
+              b2 += sizeof (uint32_t);
+              --n2;
+            }
+          tmp += sizeof (uint32_t);
+        }
       break;
     case 1:
       while (n1 > 0 && n2 > 0)
-	{
+        {
           pppppp(" *uint64_t  \n",1)
-	  if ((*cmp) (b1, b2, arg) <= 0)
-	    {
-	      *(uint64_t *) tmp = *(uint64_t *) b1;
-	      b1 += sizeof (uint64_t);
-	      --n1;
-	    }
-	  else
-	    {
-	      *(uint64_t *) tmp = *(uint64_t *) b2;
-	      b2 += sizeof (uint64_t);
-	      --n2;
-	    }
-	  tmp += sizeof (uint64_t);
-	}
+          if ((*cmp) (b1, b2, arg) <= 0)
+            {
+              *(uint64_t *) tmp = *(uint64_t *) b1;
+              b1 += sizeof (uint64_t);
+              --n1;
+            }
+          else
+            {
+              *(uint64_t *) tmp = *(uint64_t *) b2;
+              b2 += sizeof (uint64_t);
+              --n2;
+            }
+          tmp += sizeof (uint64_t);
+        }
       break;
     case 2:
       while (n1 > 0 && n2 > 0)
-	{
-	  unsigned long *tmpl = (unsigned long *) tmp;
-	  unsigned long *bl;
+        {
+          unsigned long *tmpl = (unsigned long *) tmp;
+          unsigned long *bl;
 
-	  tmp += s;
+          tmp += s;
           pppppp(" *(unsigned long *)  \n", 1)
-	  if ((*cmp) (b1, b2, arg) <= 0)
-	    {
-	      bl = (unsigned long *) b1;
-	      b1 += s;
-	      --n1;
-	    }
-	  else
-	    {
-	      bl = (unsigned long *) b2;
-	      b2 += s;
-	      --n2;
-	    }
-	  while (tmpl < (unsigned long *) tmp)
-	    *tmpl++ = *bl++;
-	}
+          if ((*cmp) (b1, b2, arg) <= 0)
+            {
+              bl = (unsigned long *) b1;
+              b1 += s;
+              --n1;
+            }
+          else
+            {
+              bl = (unsigned long *) b2;
+              b2 += s;
+              --n2;
+            }
+          while (tmpl < (unsigned long *) tmp)
+            *tmpl++ = *bl++;
+        }
       break;
     case 3:
       while (n1 > 0 && n2 > 0)
-	{
+        {
           pppppp(" *(void **)  \n", 1)
-	  if ((*cmp) (*(const void **) b1, *(const void **) b2, arg) <= 0)
-	    {
-	      *(void **) tmp = *(void **) b1;
-	      b1 += sizeof (void *);
-	      --n1;
-	    }
-	  else
-	    {
-	      *(void **) tmp = *(void **) b2;
-	      b2 += sizeof (void *);
-	      --n2;
-	    }
-	  tmp += sizeof (void *);
-	}
+          if ((*cmp) (*(const void **) b1, *(const void **) b2, arg) <= 0)
+            {
+              *(void **) tmp = *(void **) b1;
+              b1 += sizeof (void *);
+              --n1;
+            }
+          else
+            {
+              *(void **) tmp = *(void **) b2;
+              b2 += sizeof (void *);
+              --n2;
+            }
+          tmp += sizeof (void *);
+        }
       break;
     default:
       while (n1 > 0 && n2 > 0)
-	{
+        {
           pppppp(" *__mempcpy  \n", 1)
-	  if ((*cmp) (b1, b2, arg) <= 0)
-	    {
-	      tmp = (char *) __mempcpy (tmp, b1, s);
-	      b1 += s;
-	      --n1;
-	    }
-	  else
-	    {
-	      tmp = (char *) __mempcpy (tmp, b2, s);
-	      b2 += s;
-	      --n2;
-	    }
-	}
+          if ((*cmp) (b1, b2, arg) <= 0)
+            {
+              tmp = (char *) __mempcpy (tmp, b1, s);
+              b1 += s;
+              --n1;
+            }
+          else
+            {
+              tmp = (char *) __mempcpy (tmp, b2, s);
+              b2 += s;
+              --n2;
+            }
+        }
       break;
     }
 
@@ -171,16 +171,16 @@ __qsort_r (void *b, size_t n, size_t s, __compar_d_fn_t cmp, void *arg)
   if (s > 32)
     size = 2 * n * sizeof (void *) + s;
 
-    {
-      tmp = malloc (size);
-      if (tmp == NULL)
-	{
-          die("_quicksort");
-	  //_quicksort (b, n, s, cmp, arg);
-	  return;
-	}
-      p.t = tmp;
-    }
+  {
+    tmp = malloc (size);
+    if (tmp == NULL)
+      {
+        die("_quicksort");
+        //_quicksort (b, n, s, cmp, arg);
+        return;
+      }
+    p.t = tmp;
+  }
 
   p.s = s;
   p.var = 4;
@@ -196,55 +196,55 @@ __qsort_r (void *b, size_t n, size_t s, __compar_d_fn_t cmp, void *arg)
       void *tmp_storage = (void *) (tp + n);
 
       while ((void *) t < tmp_storage)
-	{
-	  *t++ = ip;
-	  ip += s;
-	}
+        {
+          *t++ = ip;
+          ip += s;
+        }
       p.s = sizeof (void *);
       p.var = 3;
       msort_with_tmp (&p, p.t + n * sizeof (void *), n);
 
       /* tp[0] .. tp[n - 1] is now sorted, copy around entries of
-	 the original array.  Knuth vol. 3 (2nd ed.) exercise 5.2-10.  */
+         the original array.  Knuth vol. 3 (2nd ed.) exercise 5.2-10.  */
       char *kp;
       size_t i;
       for (i = 0, ip = (char *) b; i < n; i++, ip += s)
-	if ((kp = tp[i]) != ip)
-	  {
-	    size_t j = i;
-	    char *jp = ip;
-	    memcpy (tmp_storage, ip, s); pppppp(" *mempcpy1  \n", 1)
+        if ((kp = tp[i]) != ip)
+          {
+            size_t j = i;
+            char *jp = ip;
+            memcpy (tmp_storage, ip, s); pppppp(" *mempcpy1  \n", 1)
 
-	    do
-	      {
-		size_t k = (kp - (char *) b) / s;
-		tp[j] = jp;
-		memcpy (jp, kp, s); pppppp(" *mempcpy2  \n", 1)
-		j = k;
-		jp = kp;
-		kp = tp[k];
-	      }
-	    while (kp != ip);
+            do
+              {
+                size_t k = (kp - (char *) b) / s;
+                tp[j] = jp;
+                memcpy (jp, kp, s); pppppp(" *mempcpy2  \n", 1)
+                j = k;
+                jp = kp;
+                kp = tp[k];
+              }
+            while (kp != ip);
 
-	    tp[j] = jp;
-	    memcpy (jp, tmp_storage, s); pppppp(" *mempcpy3  \n", 1)
-	  }
+            tp[j] = jp;
+            memcpy (jp, tmp_storage, s); pppppp(" *mempcpy3  \n", 1)
+          }
     }
   else
     {
       if ((s & (sizeof (uint32_t) - 1)) == 0
-	  && ((char *) b - (char *) 0) % __alignof__ (uint32_t) == 0)
-	{
-	  if (s == sizeof (uint32_t))
-	    p.var = 0;
-	  else if (s == sizeof (uint64_t)
-		   && ((char *) b - (char *) 0) % __alignof__ (uint64_t) == 0)
-	    p.var = 1;
-	  else if ((s & (sizeof (unsigned long) - 1)) == 0
-		   && ((char *) b - (char *) 0)
-		      % __alignof__ (unsigned long) == 0)
-	    p.var = 2;
-	}
+          && ((char *) b - (char *) 0) % __alignof__ (uint32_t) == 0)
+        {
+          if (s == sizeof (uint32_t))
+            p.var = 0;
+          else if (s == sizeof (uint64_t)
+                   && ((char *) b - (char *) 0) % __alignof__ (uint64_t) == 0)
+            p.var = 1;
+          else if ((s & (sizeof (unsigned long) - 1)) == 0
+                   && ((char *) b - (char *) 0)
+                      % __alignof__ (unsigned long) == 0)
+            p.var = 2;
+        }
       msort_with_tmp (&p, b, n);
     }
   free (tmp);
